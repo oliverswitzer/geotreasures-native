b5ab855f028e4c80b1593af6e6520c5b2e177219
Oliver Switzer <oliverswitzer@gmail.com>
2017-05-22 22:11:38 -0400

    Try to get Android map to render

--- index.android.js
+++ index.android.js
@@ -12,19 +12,62 @@ import {
   View
 } from 'react-native';
 
+import MapView from 'react-native-maps'
+import Polyline from 'polyline'
+
 export default class client extends Component {
+  constructor() {
+    super()
+
+    debugger;
+    this.state = {
+      coordinates: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyAI0Q675P0ijWtf1Cpk8pZTzgb9NL7MAGg').catch((err) => {debugger})
+      .then((response) => response.json())
+      .then((response) => {
+        const steps = Polyline.decode(response.routes[0].overview_polyline.points)
+
+        const coordinates = steps.map((element) => {
+          const latitude = parseFloat(element[0])
+          const longitude = parseFloat(element[1])
+
+          return {latitude, longitude}
+        })
+
+        this.setState({coordinates})
+      })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text>
+          Hello world
         </Text>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 45.5581968,
+            longitude: -73.8516452,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          {/*<MapView.Polyline*/}
+            {/*coordinates={this.state.coordinates}*/}
+            {/*strokeWidth={2}*/}
+            {/*strokeColor="red"*/}
+          {/*/>*/}
+          {/*<MapView.Circle center={{latitude:  37.78825, longitude: -122.4324}} radius={700} />*/}
+          {/*{this.renderPolyline()}*/}
+        </MapView>
         <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
         </Text>
       </View>
     );
@@ -33,10 +76,14 @@ export default class client extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    ...StyleSheet.absoluteFillObject,
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'stretch',
+    backgroundColor: 'gray',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
   },
   welcome: {
     fontSize: 20,
--- index.ios.js
+++ index.ios.js
@@ -4,40 +4,97 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react'
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
+import MapView from 'react-native-maps'
+import Polyline from 'polyline'
 
 export default class client extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      coordinates: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/gmaps-sample.json')
+      .then((response) => response.json())
+      .then((response) => {
+        const steps = Polyline.decode(response.routes[0].overview_polyline.points)
+
+        const coordinates = steps.map((element) => {
+          const latitude = parseFloat(element[0])
+          const longitude = parseFloat(element[1])
+
+          return {latitude, longitude}
+        })
+
+        this.setState({coordinates})
+      })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text>
+          Hello world
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 45.5581968,
+            longitude: -73.8516452,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+        <MapView.Polyline
+          coordinates={this.state.coordinates}
+          strokeWidth={2}
+          strokeColor="red"
+        />
+          {/*<MapView.Circle center={{latitude:  37.78825, longitude: -122.4324}} radius={700} />*/}
+          {/*{this.renderPolyline()}*/}
+        </MapView>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
         </Text>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  // container: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   backgroundColor: '#F5FCFF',
+  // },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
@@ -48,6 +105,6 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+})
 
-AppRegistry.registerComponent('client', () => client);
+AppRegistry.registerComponent('client', () => client)
